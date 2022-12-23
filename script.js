771bbfbbe8f5754b6e02e2731923017df8565d2c
Adam Novak <adnosk@gmail.com>
2022-12-23 15:45:40 +0100

    Add functions for pressing buttons

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 }
+
+let displayValue = 0;
+const display = document.querySelector('#number');
+
+// display the stored value on the display
+function updateDisplay() {
+    display.innerHTML = displayValue;
+};
+
+// pressing buttons and displaying the numbers on the display
+const buttons = document.querySelectorAll('.nums');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (displayValue == 0 && button.textContent !== '.') {
+            displayValue = button.textContent;
+        } else {
+            displayValue = `${displayValue}${button.textContent}`;
+        }
+        updateDisplay();
+    });
+});
+
+// AC button logic to clear the stored value
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clear => {
+    displayValue = 0;
+    updateDisplay();
+});
